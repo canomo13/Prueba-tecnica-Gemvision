0fb8aee2289393840fb8400bcbdd7c9f616842ec
José Antonio Cano Molina <canomolinajoseantonio@gmail.com>
2022-07-28 19:56:21 +0200

    ejercicio1

--- parte1.cpp
+++ parte1.cpp
@@ -1,20 +1,42 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <stdio.h>
 #include <vector>
-#include <iterator>
 
-int main(){
-  std::ifstream infile("txt/Input.txt");
-  std::vector<int> Numeros;
-  int numero;
-  while( infile >> numero)
-  {
-    Numeros.push_back(numero);
-  }
+using namespace std;
 
-  std::copy(Numeros.begin(),Numeros.end(),
-            std::ostream_iterator<int>(std::cout," "));
-  std::cout << '\n';
+int main()
+{
+    std::ofstream result("txt/resultado.txt"); 
+    std::ifstream filein("txt/Input.txt");
+    std::string line,cleanline;
+    int cont=0;
+    if (filein.is_open()){
+        
+        while(!filein.eof())
+        {
+            getline(filein,line);
+           
+        }
+        
+        std::cout << line[1] << std::endl;
+    }
 
-  return EXIT_SUCCESS;
+    line.erase(line.begin(),line.end(),",");
+    while(cont < line.size())
+    {
+        if(line[cont] == 1)
+        {
+            line[line[cont+3]]=line[line[cont+1]+line[line[cont+2]]];
+        }else if(line[cont]==2)
+        {
+            line[line[cont+3]]=line[line[cont+1]*line[line[cont+2]]];
+        }
+        cont+=4;
+    }
+    if(result.is_open()){
+        result << line << std::endl;
+    }
+   
+    filein.close();
 }
